34b814105216c42b75b9ca2749a11748a77f2e93
MikiTsa <mihail.trajkoski@student.um.si>
2025-12-19 11:47:48 +0100

    some visual fixes

--- app/src/main/java/com/example/sliknisi/MainActivity.kt
+++ app/src/main/java/com/example/sliknisi/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.example.sliknisi
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.sliknisi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigation()
+        setupHomeButton()
+        removeNavigationBadges()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Connect bottom navigation with nav controller
+        binding.bottomNavigation.setupWithNavController(navController)
+    }
+
+    private fun setupHomeButton() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.fabHome.setOnClickListener {
+            // Navigate to home fragment
+            navController.navigate(R.id.homeFragment)
         }
+    }
+
+    private fun removeNavigationBadges() {
+        // Remove any notification badges/dots from nav items
+        binding.bottomNavigation.removeBadge(R.id.achievementsFragment)
+        binding.bottomNavigation.removeBadge(R.id.mapFragment)
+        binding.bottomNavigation.removeBadge(R.id.addFragment)
+        binding.bottomNavigation.removeBadge(R.id.profileFragment)
     }
 }
